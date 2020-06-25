51d7d465adbad8771288fe5c6c993a8b979f9e44
Stefan Ober <StefanO@ppedv.de>
2020-06-25 13:07:53 +0200

    TestFiles Skript hinzugefügt als StartBasis

---
+++ Skripte/Testfiles.ps1
@@ -0,0 +1,30 @@
+﻿Param(
+    [Parameter(Mandatory=$true)]
+    $Path,
+
+    $DirCount,
+
+    $FileCount
+)
+
+$Ordner = @()
+$Ordner += New-Item -Path $Path -ItemType Directory
+
+for($i = 0;$i -lt $DirCount; $i++)
+{
+    Write-Verbose -Message "Pfad: $Path\Ordner$i "
+    Write-Debug -Message "Schleife Ordner"
+    $Ordner += New-Item -Path "$Path\Ordner$i" -ItemType Directory
+}
+
+foreach($dir in $Ordner)
+{
+    for($i = 0;$i -lt $DirCount; $i++)
+    {
+    Write-Debug -Message "schleife Dateien"
+    Write-Verbose -Message "$($dir.FullName)\Datei$i.txt"
+       #New-Item -Path ($dir.FullName + "\Datei$i.txt") -ItemType File
+       New-Item -Path "$($dir.FullName)\Datei$i.txt" -ItemType File
+    }
+}
+
