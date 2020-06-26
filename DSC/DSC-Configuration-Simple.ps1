98fc2ad5ef47f04e9393d2cd235b481c05d554dc
Stefan Ober <StefanO@ppedv.de>
2020-06-26 13:50:08 +0200

    Dsc Configuration Simple ohne configuration Data

---
+++ DSC/DSC-Configuration-Simple.ps1
@@ -0,0 +1,32 @@
+﻿configuration WebServerConfiguration
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("DC1","Member1")
+    {
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServer
+        {
+           Ensure = "Present"
+           Name   = "Web-Server"
+        }
+        WindowsFeature ManagementTools
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Tools"
+            DependsOn = "[WindowsFeature]WebServer"
+        }
+        WindowsFeature DefaultDoc
+        {
+            Ensure = "Present"
+            Name = "Web-Default-Doc"
+        }
+
+        LocalConfigurationManager
+        {
+            ConfigurationMode = "ApplyAndAutoCorrect"
+        }
+             
+    }
+}
